35cd000c9db0e2348f3c286f95cd6fe1d09b4d08
MarcusAlmert <marcus.almert@gmail.com>
2019-08-06 15:03:38 +0200

    added camera.hpp

--- framework/camera.hpp
+++ framework/camera.hpp
@@ -1,8 +1,20 @@
-//
-// Created by marcus on 05.08.19.
-//
-
 #ifndef RAYTRACER_CAMERA_HPP
 #define RAYTRACER_CAMERA_HPP
+
+#include <glm/vec3.hpp>
+#include <glm/mat4x4.hpp>
+#include <string>
+struct Camera{
+    std::string name;
+    glm::vec3 direction;
+    glm::vec3 position;
+    glm::vec3 upVector;
+    unsigned int fov;
+    glm::mat4x4 transformation =  glm::mat4x4{
+            glm::vec4{1.0f,0.0f,0.0f,0.0f},
+            glm::vec4{0.0f,1.0f,0.0f,0.0f},
+            glm::vec4{0.0f,0.0f,1.0f,0.0f},
+            glm::vec4{0.0f,0.0f,0.0f,1.0f}};
+};
 
 #endif //RAYTRACER_CAMERA_HPP
